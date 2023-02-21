49e37e30a3dcfa80b434cf22a370f5de63ed2238
rafael <rafaelcnunes@icloud.com>
2023-02-21 17:35:30 -0300

    added login

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -6,6 +9,12 @@ import store from "./store";
 import "./assets/main.css";
 
 const app = createApp(App);
+
+app.use(Toast, {
+  transition: "Vue-Toastification__bounce",
+  maxToasts: 20,
+  newestOnTop: true,
+});
 
 app.use(router);
 app.use(store);
---
+++ src/views/LoginView.vue
@@ -0,0 +1,165 @@
+<template>
+  <div id="login_view">
+    <div
+      class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+    >
+      <div class="w-full max-w-md space-y-8">
+        <div>
+          <img
+            class="mx-auto w-auto"
+            src="../assets/logo.svg"
+            alt="Your Company"
+          />
+          <h2
+            class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900"
+          >
+            Sign in to your account
+          </h2>
+          <p class="mt-2 text-center text-sm text-gray-600">
+            Or
+            <a
+              href="#"
+              class="font-medium text-indigo-600 hover:text-indigo-500"
+              >register</a
+            >
+          </p>
+        </div>
+        <div class="mt-8 space-y-6">
+          <input type="hidden" name="remember" value="true" />
+          <div class="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label for="username" class="sr-only">Username</label>
+              <input
+                id="username"
+                v-model="user.username"
+                name="username"
+                type="text"
+                autocomplete="username"
+                required
+                class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Username"
+              />
+            </div>
+            <div>
+              <label for="password" class="sr-only">Password</label>
+              <input
+                id="password"
+                v-model="user.password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required
+                class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+              />
+              <label for="remember-me" class="ml-2 block text-sm text-gray-900"
+                >Remember me</label
+              >
+            </div>
+
+            <div class="text-sm">
+              <a
+                href="#"
+                class="font-medium text-indigo-600 hover:text-indigo-500"
+                >Forgot your password?</a
+              >
+            </div>
+          </div>
+
+          <div>
+            <button
+              @click="login()"
+              class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg
+                  class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+              Sign in
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useToast } from "vue-toastification";
+import axios from "axios";
+import router from "../router";
+
+export default {
+  name: "LoginView",
+  setup() {
+    const toast = useToast();
+    return { toast };
+  },
+  data() {
+    return {
+      tasks: [],
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  computed: {
+    getUrl() {
+      return import.meta.env.VITE_API_BASE_URL;
+    },
+    accessToken() {
+      return this.$store.state.accessToken;
+    },
+  },
+  methods: {
+    async login() {
+      await axios
+        .post(`${this.getUrl}/auth/login`, {
+          username: this.user.username,
+          password: this.user.password,
+        })
+        .then((response) => {
+          this.$store.commit("setAccessToken", response.data.access_token);
+          router.push({ path: "/" });
+        })
+        .catch((error) => {
+          console.log(error);
+          this.toast.error("Login failed! Please try again.");
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+#login_view {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
